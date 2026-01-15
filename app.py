764a5b7deee5f0eec4e7887c4f9f7b81fee9e78e
rashmimalikarjun <rashmimm2006@gmail.com>
2026-01-15 17:01:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,54 @@
-from flask import Flask, render_template,request,redirect,url_for
-
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 app = Flask(__name__)
 
+# ---------------- INDEX ----------------
+# Select menu
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        menu = request.form.get("menu")
-        return redirect(url_for("order"))
+        menu = request.form.get("menu")   # READ menu here
+        return redirect(url_for("order", menu=menu))  # PASS it forward
     return render_template("index.html")
 
-    
 
+# ---------------- ORDER ----------------
+# Enter quantity
 @app.route("/order", methods=["GET", "POST"])
 def order():
     if request.method == "POST":
-        quantity = request.form["quantity"]
-        notes = request.form.get("notes")
+        selected_menu = request.form.get("menu")     # FROM hidden input
+        quantity = request.form.get("quantity")      # FROM form
+        order_time=datetime.now().strftime("%I:%M:%p | %d:%b:%Y")
+        return redirect(
+            url_for(
+                "success",
+                menu=selected_menu,
+                quantity=quantity,
+                time=order_time
+            )
+        )
 
-        print(quantity, notes)
+    # GET request (coming from index)
+    menu = request.args.get("menu")
+    return render_template("order.html", menu=menu)
 
-        return redirect(url_for("success"))
 
-    return render_template("order.html")
-
+# ---------------- SUCCESS ----------------
+# Show confirmation
 @app.route("/success")
 def success():
-    return render_template("success.html")
+    menu = request.args.get("menu")
+    quantity = request.args.get("quantity")
+    time=request.args.get("time")
+
+    return render_template(
+        "success.html",
+        menu=menu,
+        quantity=quantity,
+        time=time
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
